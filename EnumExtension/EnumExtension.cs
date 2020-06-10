3000243c1fda63f90c9ead2d3bc87673d6c077c8
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2020-06-10 21:04:53 +0800

    第一版

---
+++ EnumExtension/EnumExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumExtension
+{
+    public static class EnumExtension
+    {
+        public static string EnumNameToDescription<TEnum>(this string Name, Func<string, bool> predicate) where TEnum : Enum
+        {
+            string dpname = string.Empty;
+            try
+            {
+                Type type = typeof(TEnum);
+                if (predicate(Name))
+                {
+                    dpname = type.GetField(Name).GetCustomAttributesData()[0].ConstructorArguments[0].Value.ToString();
+                }
+            }
+            catch (Exception)
+            {
+                dpname = string.Empty;
+            }
+
+            return dpname;
+        }
+    }
+}
---
+++ EnumExtension/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EnumExtension
+{
+    public enum Customer
+    {
+        [Description("VIP")]
+        A,
+        [Description("高級顧戶")]
+        B,
+        [Description("一般顧戶")]
+        C,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "A", "B", "C" };
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"當前顧客級別： {item.EnumNameToDescription<Customer>(s => !string.IsNullOrWhiteSpace(s))}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
